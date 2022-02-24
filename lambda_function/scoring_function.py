9abdd266e88794166d7342755a1e19e3a8d7743d
Michael Lim <mikelimcontact1000@gmail.com>
2022-02-24 22:59:49 +1100

    simplify scoring lambda

--- lambda_function/scoring_function.py
+++ lambda_function/scoring_function.py
@@ -2,11 +2,10 @@ import numpy as np
 from galton.simple_linear_regr import load_model
 
 def lambda_handler(event, context):
-    data = event['payload']
     model = load_model("./artifacts/simple_linear_regression.pkl")
     predictions = [
         model.predict(np.array(row)).item()
-        for row in data['payload']
+        for row in event['payload']
     ]
     return {
         "predictions": predictions
